259b827f764abf6176dcfdf65f8948497abacf10
shaynahvilla <shaynahvilla@gmail.com>
2025-10-06 17:48:11 +0800

    title

--- villa_shainna/cad.js
+++ villa_shainna/cad.js
@@ -1,180 +1,106 @@
 var canvas;
 var gl;
-
 var maxNumTriangles = 200;
 var maxNumPositions = 3 * maxNumTriangles;
 var index = 0;
 var first = true;
-
 var t = [];
-var cIndex = 0;
+var cIndex = 0; 
 
 var colors = [
-    vec4(0.0, 0.0, 0.0, 1.0),  // black
-    vec4(1.0, 0.0, 0.0, 1.0),  // red
-    vec4(0.0, 1.0, 0.0, 1.0),  // green
-    vec4(0.0, 0.0, 1.0, 1.0),  // blue
-    vec4(1.0, 1.0, 0.0, 1.0)   // yellow
+  [1, 0.08, 0.38, 1], // Pink
+  [1, 0.5, 0, 1],     // Orange
+  [0, 0.5, 1, 1],     // Blue
+  [0, 1, 0, 1],       // Green
+  [1, 1, 0, 1]        // Yellow
 ];
 
+init();
+
 function init() {
-    canvas = document.getElementById("myCanvas");
-    gl = canvas.getContext('webgl2');
+    canvas = document.getElementById("glCanvas");
+    gl = canvas.getContext("webgl2");
     if (!gl) alert("WebGL 2.0 isn't available");
 
     gl.viewport(0, 0, canvas.width, canvas.height);
-    // set this to your preferred background color
-    gl.clearColor(0.5, 0.5, 0.5, 1.0);
+    gl.clearColor(0.96, 0.87, 0.70, 1); // nude background
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    // Load shaders and initialize attribute buffers
-    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+      var program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
-    // Creating a vertex buffer
+    // Position buffer
     var vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, 8*maxNumPositions, gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, 8 * maxNumPositions, gl.STATIC_DRAW);
 
-    // enabling the aPosition variable of the vertex shader
-    var positionLoc = gl.getAttribLocation(program, "aPosition");
+      var positionLoc = gl.getAttribLocation(program, "aPosition");
     gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(positionLoc);
 
-    // Creating a color buffer
+    // Color buffer
     var cBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, 16*maxNumPositions, gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, 16 * maxNumPositions, gl.STATIC_DRAW);
 
-    // enabling the aColor variable of the vertex shader
-    var colorLoc = gl.getAttribLocation(program, "aColor");
+      var colorLoc = gl.getAttribLocation(program, "aColor");
     gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(colorLoc);
 
-    // Making the menu work
+    // Menu selection
     var menu = document.getElementById("menu");
-    menu.addEventListener("click", function() {
+    menu.addEventListener("click", function () {
         cIndex = menu.selectedIndex;
+        console.log("Selected color index:", cIndex);
     });
 
-    // Clear button functionality
-    var clearButton = document.getElementById("clearButton");
-    clearButton.addEventListener("click", function() {
+    // Clear button
+    document.getElementById("clearButton").onclick = () => {
         index = 0;
-        first = true;
+        first = true; // reset state so next click starts fresh
         gl.clear(gl.COLOR_BUFFER_BIT);
-    });
+    };
 
-    // Establishing the Main Loop
-    canvas.addEventListener("mousedown", function(event) {
-        // Getting the position of the mouse
-        var xPos = 2 * (event.clientX/canvas.width) - 1;
-        var yPos = 2 * (canvas.height - event.clientY) / canvas.height - 1;
-        var mousePos = vec2(xPos, yPos);
+    // Mouse click → draw rectangle
+    canvas.addEventListener("mousedown", function (event) {
+        var rect = canvas.getBoundingClientRect();
+        var xPos = 2 * (event.clientX - rect.left) / canvas.width - 1;
+        var yPos = 2 * (canvas.height - (event.clientY - rect.top)) / canvas.height - 1;
+        var pos = vec2(xPos, yPos);
 
-        // Setting corners of the rectangle
-        gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+          gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
 
         if (first) {
             first = false;
-            gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-            t[0] = mousePos;
+            t[0] = pos;
         } else {
             first = true;
-            t[2] = mousePos;
+            t[2] = pos;
             t[1] = vec2(t[0][0], t[2][1]);
             t[3] = vec2(t[2][0], t[0][1]);
 
-            // Sending the corners to the GPU
-            for (var i = 0; i < 4; i++) {
-                gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index+i), flatten(t[i]));
+    // Send vertices to GPU
+      for (var i = 0; i < 4; i++) {
+      gl.bufferSubData(gl.ARRAY_BUFFER, 8 * (index + i), flatten(t[i]));
+      }
+        index += 4;
+
+    // Colors
+      gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+      var color = vec4(colors[cIndex]);
+      for (var i = 0; i < 4; ++i) {
+      gl.bufferSubData(gl.ARRAY_BUFFER, 16 * (index - 4 + i), flatten(color));
             }
-            index += 4;
-
-            // Color
-            gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-            var color = vec4(colors[cIndex]);
-            for (var i = 0; i < 4; i++) {
-                gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index-4+i), flatten(color));
-            }
         }
+      });
 
-        render();
-    });
-}
-
-function render() {
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-    for (var i = 0; i < index; i += 4) {
-        gl.drawArrays(gl.TRIANGLE_FAN, i, 4);
-    }
-
-    window.requestAnimationFrame(render);
-}
-function vec2(x, y) {
-    return [x, y];
-}
-function vec4(x, y, z, w) {
-    if (Array.isArray(x)) {
-        return [x[0], x[1], x[2], x[3]];
-    }
-    return [x, y, z, w];
-}
-
-function flatten(v) {
-    if (v.length === 2) {
-        return new Float32Array(v);
-    } else if (v.length === 4) {
-        return new Float32Array(v);
-    }
-}
-function initShaders(gl, vertexShaderId, fragmentShaderId) {
-    var vertexShader;
-    var fragmentShader;
-
-    var vertexElement = document.getElementById(vertexShaderId);
-    if (!vertexElement) {
-        alert("Unable to load vertex shader " + vertexShaderId);
-        return -1;
-    } else {
-        vertexShader = gl.createShader(gl.VERTEX_SHADER);
-        gl.shaderSource(vertexShader, vertexElement.textContent.replace(/^\s+|\s+$/g, ''));
-        gl.compileShader(vertexShader);
-        if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-            var msg = "Vertex shader failed to compile. The error log is:"
-                + "<pre>" + gl.getShaderInfoLog(vertexShader) + "</pre>";
-            alert(msg);
-            return -1;
+    // Render loop
+    function render() {
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        for (var i = 0; i < index; i += 4) {
+            gl.drawArrays(gl.TRIANGLE_FAN, i, 4);
         }
+        window.requestAnimationFrame(render);
     }
-    var fragmentElement = document.getElementById(fragmentShaderId);
-    if (!fragmentElement) {
-        alert("Unable to load fragment shader " + fragmentShaderId);
-        return -1;
-    } else {
-        fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-        gl.shaderSource(fragmentShader, fragmentElement.textContent.replace(/^\s+|\s+$/g, ''));
-        gl.compileShader(fragmentShader);
-        if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-            var msg = "Fragment shader failed to compile. The error log is:"
-                + "<pre>" + gl.getShaderInfoLog(fragmentShader) + "</pre>";
-            alert(msg);
-            return -1;
-        }
-    }
-    var program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-        var msg = "Shader program failed to link. The error log is:"
-            + "<pre>" + gl.getProgramInfoLog(program) + "</pre>";
-        alert(msg);
-        return -1;
-    }
-
-    return program;
+    render();
 }
-window.onload = init;
